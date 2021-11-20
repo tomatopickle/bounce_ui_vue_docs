a48acf91d961bdacd28706034bee1725bebb6419
tomatopickle <abaanashanid@gmail.com>
2021-11-20 18:27:00 +0300

    added icons and stuff

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,74 +1,102 @@
 module.exports = {
-  title: 'Bounce UI',
-  base: "/",
+  title: "Bounce UI",
+  base: "/bounce_ui_vue_docs/",
   head: [
     [
       "link",
       {
         rel: "stylesheet",
-        href: "/dist/bounce_ui.css"
-      }
+        href: "/dist/bounce_ui.css",
+      },
     ],
     [
       "link",
       {
         rel: "preconnect",
-        href: "https://fonts.googleapis.com"
-      }
+        href: "https://fonts.googleapis.com",
+      },
     ],
     [
       "link",
       {
         rel: "preconnect",
-        href: "https://fonts.gstatic.com"
-      }
+        href: "https://fonts.gstatic.com",
+      },
     ],
     [
       "link",
       {
         rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700&display=swap"
-      }
+        href: "https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700&display=swap",
+      },
     ],
     [
       "link",
       {
         rel: "stylesheet",
-        href: "https://cdnjs.cloudflare.com/ajax/libs/gridlex/2.7.1/gridlex.min.css"
-      }
+        href: "https://cdnjs.cloudflare.com/ajax/libs/gridlex/2.7.1/gridlex.min.css",
+      },
     ],
     [
       "link",
       {
         rel: "stylesheet",
-        href: "https://cdn.jsdelivr.net/npm/@mdi/font@6.4.95/css/materialdesignicons.min.css"
-      }
-    ]
+        href: "https://cdn.jsdelivr.net/npm/@mdi/font@6.4.95/css/materialdesignicons.min.css",
+      },
+    ],
+    ["link", { rel: "manifest", href: "/site.webmanifest" }],
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/favicons/apple-touch-icon.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicons/favicon-32x32.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicons/favicon-16x16.png",
+      },
+    ],
+    ["link", { rel: "shortcut icon", href: "/favicon.ico" }],
   ],
   themeConfig: {
-    logo: '/images/logo.png',
+    logo: "/images/logo.png",
     navbar: [
       {
-        text: 'Components',
-        link: '/components',
+        text: "Components",
+        link: "/components",
       },
       {
-        text: 'Guide',
-        link: '/guide/',
-      }
+        text: "Guide",
+        link: "/guide/",
+      },
     ],
   },
   plugins: [
-    ['vuepress-plugin-demoblock-plus'],
+    ["vuepress-plugin-demoblock-plus"],
     [
-      '@vuepress/plugin-search',
+      "@vuepress/plugin-search",
       {
         locales: {
-          '/': {
-            placeholder: 'Search',
-          }
+          "/": {
+            placeholder: "Search",
+          },
         },
       },
     ],
-  ]
-}
+  ],
+};
